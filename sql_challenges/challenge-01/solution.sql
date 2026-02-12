95087f77619727f5fac59f5da56cf6d4e6e893da
Javier272 <147571233+Javier272@users.noreply.github.com>
2026-02-12 08:24:29 -0600

    Comments of sesion 1

---
+++ sql_challenges/challenge-01/solution.sql
@@ -0,0 +1,14 @@
+-- lesson 1
+SELECT title FROM movies;
+
+-- lesson 2
+SELECT director FROM movies;
+
+-- lesson 3
+SELECT title, director FROM movies;
+
+-- lesson 4
+SELECT title, year FROM movies;
+
+-- lesson 5
+SELECT * FROM movies;

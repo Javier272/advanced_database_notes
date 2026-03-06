51837fee76c71d15f0a787352b80cf833aa0a5ab
Javier272 <147571233+Javier272@users.noreply.github.com>
2026-03-05 20:52:54 -0600

    2

--- sql_challenges/challenge-02/solution.sql
+++ sql_challenges/challenge-02/solution.sql
@@ -1,7 +1,6 @@
 -- lession 6
 SELECT Domestic_sales, International_sales, Title FROM Boxoffice JOIN Movies ON Boxoffice.Movie_id = Movies.id;
 
-
 SELECT Domestic_sales, International_sales, Title FROM Boxoffice JOIN Movies ON Movie_id = Id WHERE Domestic_sales < International_Sales;
 
 SELECT Title, Rating FROM Movies JOIN Boxoffice ON Id = Movie_id ORDER BY Rating DESC;
@@ -15,3 +14,4 @@ SELECT DISTINCT Building_name, Role FROM Buildings LEFT JOIN Employees ON Buildi
 
 -- question
 SELECT  p.page_id FROM pages p LEFT JOIN page_likes pl ON p.page_id = pl.page_id WHERE pl.page_id IS NOT NULL ORDER BY p.page_id DESC;
+
